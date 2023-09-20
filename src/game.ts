39103e0b7cd6276e540c33fa827274bd1c40dd1c
Andy O'Neill <aoneill01@gmail.com>
2023-09-19 20:09:54 -0400

    Fly in enemies

--- src/game.ts
+++ src/game.ts
@@ -6,20 +6,7 @@ export class Game {
     player: Player = new Player();
     controls: Controls = new Controls();
     bullets: Bullet[] = [];
-    enemies: Enemy[] = messageToEnemies([
-        {
-            text: "PRESS ",
-            color: 0,
-        },
-        {
-            text: "SPACE ",
-            color: 1,
-        },
-        {
-            text: "TO FIRE",
-            color: 0,
-        },
-    ]);
+    enemies: Enemy[] = getEnemies();
     stars: Star[] = [];
     particles: Particle[] = [];
 
@@ -45,10 +32,7 @@ export class Game {
             this.bullets.push(new Bullet(this.player.x + 7, this.player.y));
         }
 
-        const tmp = this.tickCount % 256;
-        let offset = (tmp < 128 ? tmp : 256 - tmp) / 4 - 16;
-
-        this.enemies.forEach((enemy) => enemy.updatePosition(offset));
+        this.enemies.forEach((enemy) => enemy.tick(this.tickCount));
 
         this.bullets.forEach((bullet) => {
             bullet.tick();
@@ -68,7 +52,7 @@ export class Game {
     createExplosion(x: number, y: number, color: number) {
         for (let i = 0; i < 8; i++) {
             const angle = (Math.PI / 3) * Math.random() - Math.PI / 1.5;
-            const velocity = Math.random() / 2 + 0.5;
+            const velocity = Math.random() / 2 + 1;
             this.particles.push(
                 new Particle(
                     x + Math.random() * 2,
@@ -99,23 +83,71 @@ class Player extends Sprite {
 }
 
 class Enemy extends Sprite {
+    row: number;
     column: number;
     spriteY: number;
     spriteX: number;
+    private mode: string = "waiting";
+    private targetY?: number;
+    private targetX?: number;
+    private startTickCount?: number;
 
-    constructor(column: number, spriteY: number, spriteX: number) {
-        super(0, 16 * 8);
+    constructor(row: number, column: number, spriteY: number, spriteX: number) {
+        super(BOARD_HEIGHT, BOARD_WIDTH);
+        this.row = row;
         this.column = column;
         this.spriteY = spriteY;
         this.spriteX = spriteX;
     }
 
-    updatePosition(offset: number) {
-        this.x = offset + 8 * this.column;
+    tick(tickCount: number) {
+        const START_Y = 23 * 8;
+        const FLY_IN_TICKS = 100;
+        const P1 = {
+            x: this.row % 2 === 0 ? -8 : BOARD_WIDTH,
+            y: 8 * 4,
+        };
+        const P2 = {
+            x: this.row % 2 === 0 ? BOARD_WIDTH : 0,
+            y: 0,
+        };
+
+        switch (this.mode) {
+            case "waiting":
+                if (this.calculateY(tickCount) < START_Y) {
+                    this.targetY = this.calculateY(tickCount + FLY_IN_TICKS + this.column * 8);
+                    this.targetX = this.calculateX(tickCount + FLY_IN_TICKS + this.column * 8);
+                    this.startTickCount = tickCount + this.column * 8;
+                    this.mode = "flyIn";
+                }
+
+                break;
+            case "flyIn":
+                const t = (tickCount - this.startTickCount!) / FLY_IN_TICKS;
+                this.y = (1 - t) ** 2 * P1.y + 2 * (1 - t) * t * P2.y + t ** 2 * this.targetY!;
+                this.x = (1 - t) ** 2 * P1.x + 2 * (1 - t) * t * P2.x + t ** 2 * this.targetX!;
+                if (t >= 1) this.mode = "group";
+                break;
+            case "group":
+                this.y = this.calculateY(tickCount);
+                this.x = this.calculateX(tickCount);
+                break;
+        }
     }
 
     isHit(x: number, y: number): boolean {
         return x >= this.x && x < this.x + 8 && y >= this.y && y < this.y + 8;
+    }
+
+    calculateY(tickCount: number) {
+        const slowedCount = Math.floor(tickCount / 16);
+        return 20 * 8 - slowedCount + 16 * this.row;
+    }
+
+    calculateX(tickCount: number) {
+        const slowedCount = Math.floor(tickCount / 4);
+        const remainder = slowedCount % 64;
+        return (remainder < 32 ? remainder : 64 - remainder) - 16 + 8 * this.column;
     }
 }
 
@@ -175,7 +207,51 @@ type MessageBlock = {
     color: number;
 };
 
-function messageToEnemies(message: MessageBlock[]): Enemy[] {
+function getEnemies(): Enemy[] {
+    const result = [];
+    const text = `Welcome My name is
+*Andy ONeill*
+
+*Space* to fire
+*x* and *x* to move
+
+I am a
+software engineer
+
+Check out my code
+on *Github*
+
+Find more info
+on *LinkedIn*
+
+I enjoy
+retro gaming
+mechanical keyboards
+electronics
+origami
+
+I worked at
+*Scratch*`.toUpperCase();
+
+    const lines = text.split("\n");
+    for (let row = 0; row < lines.length; row++) {
+        const blocks = lines[row].split("*");
+        let color = 0;
+        const message = [];
+        for (const block of blocks) {
+            message.push({
+                text: block,
+                color,
+            });
+            color = (color + 1) % 2;
+        }
+        result.push(...messageToEnemies(message, row));
+    }
+
+    return result;
+}
+
+function messageToEnemies(message: MessageBlock[], row: number): Enemy[] {
     const result: Enemy[] = [];
     const totalLength = message.reduce((sum, mb) => sum + mb.text.length, 0);
     let i = Math.floor((28 - totalLength) / 2);
@@ -186,7 +262,7 @@ function messageToEnemies(message: MessageBlock[]): Enemy[] {
                 continue;
             }
             const y = letter.charCodeAt(0) - "A".charCodeAt(0);
-            result.push(new Enemy(i, y, mb.color));
+            result.push(new Enemy(row, i, y, mb.color));
             i++;
         }
     }
